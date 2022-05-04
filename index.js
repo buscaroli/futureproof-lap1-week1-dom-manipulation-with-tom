0c9a56bf7466db4ba9ce733eff7205058e3887e7
mabus <m.buscaroli@outlook.com>
2022-05-04 12:23:52 +0100

    removed a tags and added some queryselectors in index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const header1 = document.querySelector('h1')
+const header2 = document.querySelector('h2')
+
+const link1 = document.querySelector('#link-1')
+const link2 = document.querySelector('#link-2')
+const link3 = document.querySelector('#link-3')
+
+console.log(header1)
+console.log(header2)
+console.log(link1)
+console.log(link2)
+console.log(link3)
