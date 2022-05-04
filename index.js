44335d0baf372e9f20387bfc34504bbaf33a10ea
mabus <m.buscaroli@outlook.com>
2022-05-04 14:14:08 +0100

    recording key pressed in new paragraphs

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const link3 = document.querySelector('#link-3')
 
 const section2 = document.querySelector('#section-2')
 
+const paragraph1 = document.querySelector('#paragraph-1')
+const paragraph2 = document.querySelector('#paragraph-2')
+const paragraph3 = document.querySelector('#paragraph-3')
+
 // console.log(header1)
 // console.log(header2)
 // console.log(link1)
@@ -24,17 +28,54 @@ header1.addEventListener('mouseleave', () => {
 })
 
 // change text of h2 when clicked
-header2.addEventListener('click', () => {
+header2.addEventListener('mouseover', () => {
   header2.textContent = 'Hello and Welcome!'
 })
 
 // add a paragraph to section-2
 document.addEventListener('keypress', addParagraph)
-function addParagraph() {
+function addParagraph(e) {
+  console.log()
   let newParagraph = document.createElement('p')
-  newParagraph.textContent = `new paragraph!`
+  newParagraph.textContent = String.fromCharCode(e.keyCode)
   return section2.appendChild(newParagraph)
 }
 
 // change BG colour of paragraphs
-link1.addEventListener('click', (e) => {})
+link1.addEventListener('click', () => {
+  // styleToggle(paragraph1)
+  console.log(paragraph1)
+
+  let fontSize = window
+    .getComputedStyle(paragraph1, null)
+    .getPropertyValue('font-size')
+  console.log('defaultSize: ', fontSize)
+
+  if (paragraph1.fontSize === fontSize) {
+    paragraph1.style.fontSize = '40px'
+  } else {
+    paragraph1.style.fontSize = fontSize
+  }
+
+  console.log('newSize: ', paragraph1.fontSize)
+})
+
+function styleToggle(element) {
+  let fontSize = window
+    .getComputedStyle(element, null)
+    .getPropertyValue('font-size')
+  let fontWeight = window
+    .getComputedStyle(element, null)
+    .getPropertyValue('font-weight')
+  console.log(fontSize)
+  // console.log(fontWeight)
+
+  element.fontSize === fontSize
+    ? (element.style.fontSize = '40px')
+    : (element.style.fontSize = fontSize)
+  console.log('newSize: ', element.fontSize)
+
+  element.fontWeight === fontWeight
+    ? (element.style.fontWeight = 'bold')
+    : (element.style.fontWeight = fontWeight)
+}
